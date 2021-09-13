8138a60a9d17b58c800856a033def63925ff9975
NagaiKoki <kokinagai10+1@gmail.com>
2021-09-13 19:20:22 +0900

    add boilers

---
+++ pages/todos/[todo_id].tsx
@@ -0,0 +1,8 @@
+import { VFC } from "react";
+import { TodoEditContainer } from "../../src/components/Todo/EditContainer";
+
+const TodoPage: VFC = () => {
+  return <TodoEditContainer />;
+};
+
+export default TodoPage;
