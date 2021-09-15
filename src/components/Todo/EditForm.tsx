eebfd085f3a9b3362f2142b36f284d51695b71c6
NagaiKoki <kokinagai10+1@gmail.com>
2021-09-15 15:35:41 +0900

    fix

--- src/components/Todo/EditForm.tsx
+++ src/components/Todo/EditForm.tsx
@@ -12,9 +12,20 @@ export const TodoEditForm: VFC<Props> = ({
   onRequestPatchTodo,
 }) => {
   return (
-    <div style={{ padding: "40px" }}>
-      <label>タイトル</label>
-      <input value={title} onChange={(e) => onChangeTitle(e.target.value)} />
+    <div
+      style={{
+        padding: "40px",
+        display: "flex",
+        flexDirection: "column",
+        width: "400px",
+      }}
+    >
+      <div style={{ marginBottom: "10px " }}>
+        <label>タイトル</label>
+      </div>
+      <div style={{ marginBottom: "20px" }}>
+        <input value={title} onChange={(e) => onChangeTitle(e.target.value)} />
+      </div>
       <button type="submit" onClick={() => onRequestPatchTodo({ title })}>
         送信
       </button>
